5300ae63b6e8ec366f463b5e42e0d3d370038355
Karimi-F <fridah37karimi@gmail.com>
2024-11-07 20:23:00 +0300

    Fetches random dogs

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, { useEffect } from "react";
+
+const App = () => {
+    const[dogImage,setDogImage]=React.useState(null);
+    const[isLoaded,setIsLoaded]=React.useState(false);
+
+useEffect(()=>{
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then((response) => response.json())
+    .then((data)=>{
+        setDogImage(data.message);
+        setIsLoaded(true);
+    })
+    },[])
+
+    if (!isLoaded) return <p>Loading...</p>;
+
+  return (<div>
+    App
+    <img src={dogImage} alt="A Random Dog"/>
+  </div>);
+};
+
+
+
+export default App;
